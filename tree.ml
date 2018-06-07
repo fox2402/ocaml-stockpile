d52a1127e67b2c3523f4029cb57459c8575c1eb7
Kenan <kenan.lejosne@gmail.com>
2018-06-08 01:59:45 +0200

    tree: height and bst_push

--- tree.ml
+++ tree.ml
@@ -1,3 +1,17 @@
-type tree = Node of tree * tree | Leaf of int;;
+type 'a tree = Node of 'a * 'a tree * 'a tree | Leaf;;
 
+(*common tree *)
 
+let rec height t = match t with
+  | Node (v, l, r) -> 1 + max (height l) (height r) 
+  | Leaf -> -1;;
+
+(* BST *)
+
+let rec bst_push t value = match t with
+  | Node (v, l, r) -> if v > value 
+                      then Node (v, bst_push l value, r) 
+                      else Node (v, l, bst_push r value)
+  | Leaf -> Node (value, Leaf, Leaf);;
+
+ 
