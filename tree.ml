ba42af14ab2d051af22a885efb120aec4f3b85a2
Kenan Lejosne <keann.lejosne@gmail.com>
2018-06-10 12:29:22 +0200

    tree : bst_delete

--- tree.ml
+++ tree.ml
@@ -9,9 +9,9 @@ let rec height t = match t with
 (* BST *)
 let rec bst_find_min t = match t with
   | Node (v, l, r) -> (match l with
-                      | Node -> bst_find_min l
-                      | Leaf -> v
-  | Leaf -> -1
+                      | Node (_,_,_) -> bst_find_min l
+                      | Leaf -> v)
+  | Leaf -> -1;;
 
 let rec bst_push t value = match t with
   | Node (v, l, r) -> if v > value 
@@ -27,20 +27,33 @@ let rec bst_search t value = match t with
                           bst_search l value
                         else
                           bst_search r value
-  | Leaf -> Leaf
+  | Leaf -> Leaf;;
 
 
 let rec bst_delete t value = match t with
-  | Node (v, l, r) -> if v < value then
+  | Node (v, l, r) -> if v > value then
                         let (res, va) = bst_delete l value in
                         (Node (v, res, r), va)
-                      if v > value then
-                        let (res, va) = bst_delete r value in
-                        (Node (v, l, res), va)
-                      else
-                        (match l, r with
-                        | (Node, Leaf) -> (l, v)
-                        | (Leaf, Node) -> (r, v)
-                        | _, _ ->
+                      else 
+                        if v < value then
+                          let (res, va) = bst_delete r value in
+                          (Node (v, l, res), va)
+                        else
+                          (match l, r with
+                          | (Node (_, _, _), Leaf) -> (l, v)
+                          | (Leaf, Node (rv, rl, rr)) -> (r, v)
+                          | (Leaf, Leaf) -> (Leaf, v)
+                          | _, _ -> let minimum = bst_find_min r in
+                          let (del_res, va) = bst_delete r minimum in
+                          (Node (va, l, del_res), value))
             
-  | Leaf -> (Leaf, -1)
+  | Leaf -> (Leaf, -1);;
+
+
+let t = Leaf;;
+let t = bst_push t 8;;
+let t = bst_push t 4;;
+let t = bst_push t 12;;
+let t = bst_push t 1;;
+let t = bst_push t 5;;
+let t = bst_push t 13;;
